f3ab67469ce881a8960ee6e579902045c00808f1
gladki24 <seweryngla@hotmail.com>
2021-10-24 18:57:11 +0200

    #3 add login/registration forms

--- src/app/modules/shared/shared.module.ts
+++ src/app/modules/shared/shared.module.ts
@@ -1,12 +1,25 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import {MatCardModule} from "@angular/material/card";
+import { InputComponent } from './components/input/input.component';
+import { MatFormFieldModule } from "@angular/material/form-field";
+import { MatInputModule } from "@angular/material/input";
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+
+    InputComponent
+  ],
+  exports: [
+    InputComponent
+  ],
   imports: [
-    CommonModule
+    CommonModule,
+    MatCardModule,
+    MatFormFieldModule,
+    MatInputModule
   ]
 })
 export class SharedModule { }
--- src/app/modules/welcome/welcome.module.ts
+++ src/app/modules/welcome/welcome.module.ts
@@ -1,13 +1,32 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { WelcomeRoutingModule } from "./welcome-routing.module";
+import { WelcomeComponent } from './pages/welcome/welcome.component';
+import { LoginComponent } from './pages/login/login.component';
+import { RegisterComponent } from './pages/register/register.component';
+import { MatCardModule } from "@angular/material/card";
+import { MatDividerModule } from "@angular/material/divider";
+import { MatButtonModule } from "@angular/material/button";
+import { MatFormFieldModule } from "@angular/material/form-field";
+import { MatInputModule } from "@angular/material/input";
+import { SharedModule } from "../shared/shared.module";
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    WelcomeComponent,
+    LoginComponent,
+    RegisterComponent
+  ],
   imports: [
     CommonModule,
-    WelcomeRoutingModule
+    WelcomeRoutingModule,
+    MatCardModule,
+    MatDividerModule,
+    MatButtonModule,
+    MatFormFieldModule,
+    MatInputModule,
+    SharedModule
   ]
 })
 export class WelcomeModule {
